80326d5592aac8a3df3ec5a5c15efef160aa3103
pand32 <100540070+pand32@users.noreply.github.com>
2023-10-25 18:23:28 -0300

    Add files via upload

---
+++ Trabalho1/MVC/Model/classe_endereco.php
@@ -0,0 +1,110 @@
+<?php
+
+class Endereco {
+    // atributos
+    private int $id_endereco;
+    private int $id_cliente;
+    private string $logradouro;
+    private string $numero;
+    private string $complemento;
+    private string $cep;
+    private string $bairro;
+    private string $cidade;
+    private string $estado;
+    private string $tipo;
+
+
+    // metodos
+    // Construtor
+    
+    // Construtor com valores pre-estabelecidos vazios
+    function __construct(){
+        $this->id_endereco = 0;
+        $this->id_cliente = 0;
+        $this->logradouro = "";
+        $this->numero = "";
+        $this->complemento = "";
+        $this->cep = "";
+        $this->bairro = "";
+        $this->cidade = "";
+        $this->estado = "";
+        $this->tipo = "";
+
+        echo "Criei um objeto da classe Endereço "._CLASS_;
+    }
+
+    // Gets
+    function GetId_endereco(){
+        return $this->id_endereco;
+    }
+
+    // Sets
+    function SetId_endereco(int $pId_endereco){
+        $this->id_endereco = $pId_endereco;
+    }
+    
+    function GetId_cliente(){
+        return $this->id_cliente;
+    }
+
+    function SetId_cliente(int $pId_cliente){
+        $this->id_cliente = $pId_cliente;
+    }
+
+    function GetLogradouro(){
+        return $this->logradouro;
+    }
+
+    function SetLogradouroo(string $pLogradouro){
+        $this->logradouro = $pLogradouro;
+    }
+    function GetNumero(){
+        return $this->numero;
+    }
+
+    function SetNumero(string $pNumero){
+        $this->numero = $pNumero;
+    }
+    function GetComplemento(){
+        return $this->complemento;
+    }
+    function SetComplemento(string $Complemento){
+        $this->complemento = $pComplemento;
+    }
+    function GetCep(){
+        return $this->cep;
+    }
+    function SetCep(string $Cep){
+        $this->cep= $pCep;
+    }
+    function GetBairro(){
+        return $this->bairro;
+    }
+    function SetBairro(string $Bairro){
+        $this->bairro= $pBairro;
+    }
+
+    function GetCidade(){
+        return $this->cidade;
+    }
+    function SetCidade(string $Cidade){
+        $this->cidade= $pCidade;
+    }
+
+    function GetEstado(){
+        return $this->estado;
+    }
+    function SetEstado(string $Estado){
+        $this->estado= $pEstado;
+    }
+
+    function GetTipo(){
+        return $this->tipo;
+    }
+    function SetTipo(string $Tipo){
+        $this->tipo= $pTipo;
+    }
+
+
+// Criando primeiro objeto da classe usando a palavra reservada NEW
+$endereco = new Endereco;
---
+++ Trabalho1/MVC/Model/classe_produto.php
@@ -0,0 +1,103 @@
+<?php
+
+class Produto {
+   
+    private int $id_produto;
+    private string $desc_produto;
+    private int $id_modelo;
+    private float $capacidade;
+    private float $vlr_sugerido;
+    private float $vlr_custo;
+    private string $voltagem;
+    private int $id_cor;
+     
+    
+    function __construct(){
+        $this->id_produto = 0;
+        $this->desc_produto = "";
+        $this->id_modelo = 0;
+        $this->capacidade = 0.0;
+        $this->vlr_sugerido = 0.0;
+        $this->vlr_custo = 0.0;
+        $this->voltagem = "";
+        $this->id_cor = 0;
+
+        echo "Criei um objeto da classe Produto "._CLASS_;
+    }
+
+    
+    function GetId_produto(){
+        return $this->id_produto;
+    }
+
+    
+    function SetId_produto(int $pId_produto){
+        $this->id_produto = $pId_produto;
+    }
+
+    function GetDesc_produto(){
+        return $this->desc_produto;
+    }
+
+    
+    function SetDesc_produto(string $pDesc_produto){
+        $this->desc_produto = $pDesc_produto;
+    }
+
+    function GetId_modelo(){
+        return $this->id_modelo;
+    }
+
+    
+    function SetId_modelo(int $pId_modelo){
+        $this->id_modelo = $pId_modelo;
+    }
+
+    function GetCapacidade(){
+        return $this->capacidade;
+    }
+
+    
+    function SetCapacidade(float $pCapacidade){
+        $this->capacidade = $pCapacidade;
+    }
+
+    function GetVlr_sugerido(){
+        return $this->vlr_sugerido;
+    }
+
+    
+    function SetVlr_sugerido(float $pVlr_sugerido){
+        $this->vlr_sugerido = $pVlr_sugerido;
+    }
+
+    function GetVlr_custo(){
+        return $this->vlr_custo;
+    }
+
+    
+    function SetVlr_custo(float $pVlr_custo){
+        $this->vlr_custo = $pVlr_custo;
+    }
+
+    function GetVoltagem(){
+        return $this->voltagem;
+    }
+
+    
+    function SetVoltagem(string $pVoltagem){
+        $this->voltagem = $pVoltagem;
+    }
+
+    function GetId_cor(){
+        return $this->id_cor;
+    }
+
+    
+    function SetId_cor(int $pId_cor){
+        $this->id_cor = $pId_cor;
+    }
+}
+
+
+$produto = new Produto;
