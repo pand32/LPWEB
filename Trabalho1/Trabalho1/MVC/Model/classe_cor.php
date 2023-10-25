876ab9660e2e7c5e7de3ca29b76898df6c2ac803
pand32 <100540070+pand32@users.noreply.github.com>
2023-10-25 18:17:14 -0300

    Add files via upload

---
+++ Trabalho1/Trabalho1/MVC/Model/classe_cor.php
@@ -0,0 +1,31 @@
+<?php
+
+class Cor {
+
+    private int $id_cor;
+    private string $desc_cor;
+
+    function __construct(){
+        $this->id_cor = 0;
+        $this->desc_cor = "";
+
+        echo "Criei um objeto da classe Cor "._CLASS_;
+    }
+    function GetId_cor(){
+        return $this->id_cor;
+    }
+
+    function SetId_cor(int $pId_cor){
+        $this->id_cor = $pId_cor;
+    }
+
+    function GetDesc_cor(){
+        return $this->desc_cor;
+    }
+
+    function SetDesc_cor(string $pDesc_cor){
+        $this->desc_cor = $pDesc_cor;
+    }
+
+    $Cor1 = new Cor;
+}
